64bab60a160f6f441d96427bcf28dd518b91f80f
adeptex <arteaocho@gmail.com>
2021-10-27 21:00:58 +0200

    Improve private key detection

--- whispers/plugins/plaintext.py
+++ whispers/plugins/plaintext.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Iterator
+from typing import Iterator, Optional
 
 from whispers.core.utils import KeyValuePair, is_uri, strip_string
 from whispers.plugins.uri import Uri
@@ -8,11 +8,32 @@ from whispers.plugins.uri import Uri
 class Plaintext:
     def pairs(self, filepath: Path) -> Iterator[KeyValuePair]:
         for lineno, line in enumerate(filepath.open(), 1):
-            if not strip_string(line):
+            line = strip_string(line)
+            if not line:
                 continue
 
-            for value in line.split():
-                if is_uri(value):
-                    for pair in Uri().pairs(value):
-                        pair.line = lineno
-                        yield pair
+            yield from self.uri_pairs(line, lineno)
+            yield from self.privatekey_pairs(line, lineno)
+
+    @staticmethod
+    def privatekey_pairs(line: str, lineno: int) -> Optional[Iterator[KeyValuePair]]:
+        if not (line.startswith("---") and line.endswith("---")):
+            return None
+
+        if len(line) < 12:
+            return None
+
+        yield KeyValuePair("key", line, line=lineno)
+
+    @staticmethod
+    def uri_pairs(line: str, lineno: int) -> Optional[Iterator[KeyValuePair]]:
+        if "://" not in line:
+            return None
+
+        for value in line.split():
+            if not is_uri(value):
+                continue
+
+            for pair in Uri().pairs(value):
+                pair.line = lineno
+                yield pair
