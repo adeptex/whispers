248da74af550b174c89eb4f26236c14e3e2be1f4
adeptex <arteaocho@gmail.com>
2022-05-02 21:33:53 +0200

    Python 3.6 dataclasses

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from importlib import import_module
 from setuptools import find_packages, setup
 from pathlib import Path
+from sys import version_info
 
 
-install_requires = ["dataclasses", "luhn", "lxml", "pyyaml", "astroid", "jproperties", "jellyfish", "beautifulsoup4"]
+install_requires = ["luhn", "lxml", "pyyaml", "astroid", "jproperties", "jellyfish", "beautifulsoup4"]
+if version_info < (3, 7):  # Python 3.6 requirements
+    install_requires.append("dataclasses")
 
 dev_requires = [
     "autoflake~=1.4",
