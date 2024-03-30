7f7d5d16c599ea9be1a0f32b7e6430c2bd6a2995
adeptex <adeptex@users.noreply.github.com>
2024-03-30 01:06:14 +0100

    Update deprecated argument

--- whispers/plugins/html.py
+++ whispers/plugins/html.py
@@ -11,7 +11,7 @@ class Html:
     def pairs(self, filepath: Path) -> Iterator[KeyValuePair]:
         soup = BeautifulSoup(filepath.read_text(), "lxml")
         key = "comment"
-        for comment in soup.find_all(text=lambda text: isinstance(text, Comment)):
+        for comment in soup.find_all(string=lambda text: isinstance(text, Comment)):
             comment = truncate_all_space(comment.extract()).strip()
             if comment:
                 yield KeyValuePair(key, comment, [key])
