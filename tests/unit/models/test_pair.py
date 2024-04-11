8621e68b5c5f9fbdf5d7925b3c1a7c9a4d6c1dce
adeptex <adeptex@users.noreply.github.com>
2024-04-12 01:10:19 +0200

    Update fixture test

--- tests/unit/models/test_pair.py
+++ tests/unit/models/test_pair.py
@@ -1,15 +1,24 @@
 from whispers.models.pair import KeyValuePair
 
 
-def test_pair():
-    pair = KeyValuePair("key", "value", ["key", "path"], "file", 123, {"rule_id": "test"})
+def test_pair(rule_fixture):
+    pair = KeyValuePair("key", "value", ["key", "path"], "file", 123, rule_fixture)
     assert pair.__dict__ == {
         "key": "key",
         "value": "value",
         "keypath": ["key", "path"],
         "file": "file",
         "line": 123,
-        "rule": {"rule_id": "test"},
+        "rule": rule_fixture,
+    }
+    assert pair.to_json() == {
+        "key": "key",
+        "value": "value",
+        "file": "file",
+        "line": 123,
+        "rule_id": "fixture",
+        "message": "test",
+        "severity": "Info",
     }
 
 
