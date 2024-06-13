307aef7966cbf7420f0b65ff8bc8cdd128c26663
adeptex <adeptex@users.noreply.github.com>
2024-06-13 19:58:26 +0200

    Requirements

--- setup.py
+++ setup.py
@@ -1,54 +1,28 @@
 from importlib import import_module
-from setuptools import find_packages, setup
 from pathlib import Path
-from sys import version_info
+
+from setuptools import find_packages, setup
+
+VERSION = import_module("whispers.__version__").__version__
 
 
-install_requires = [
-    "astroid",
-    "beautifulsoup4",
-    "crossplane",
-    "jellyfish",
-    "jproperties",
-    "luhn",
-    "lxml",
-    "pyyaml",
-]
-
-dev_requires = [
-    "autoflake~=1.4",
-    "autopep8~=1.7",
-    "black==22.8; python_version <= '3.6'",
-    "black~=24.3; python_version > '3.6'",
-    "build~=0.8",
-    "coverage~=4.5",
-    "coverage-badge~=1.0",
-    "flake8~=5.0",
-    "isort~=5.9",
-    "pytest~=7.0",
-    "pytest-mock~=3.6",
-    "pip-tools~=6.2",
-    "wheel~=0.37",
-    "twine~=3.4",
-]
+def res(filename: str) -> str:
+    """Read package resource"""
+    return Path(__file__).parent.joinpath(filename).read_text()
 
 
-def get_version():
-    return import_module("whispers.__version__").__version__
-
-
-def get_readme():
-    return Path(__file__).parent.joinpath("README.md").read_text()
-
+install_requires = res("requirements.txt").split("\n")
+dev_requires = res("requirements-dev.txt").split("\n")
+readme = res("README.md")
 
 setup(
     name="whispers",
-    version=get_version(),
+    version=VERSION,
     url="https://github.com/adeptex/whispers",
     author="Artëm Tsvetkov",
     author_email="adeptex@users.noreply.github.com",
     description="Identify secrets in static structured text",
-    long_description=get_readme(),
+    long_description=readme,
     long_description_content_type="text/markdown",
     packages=find_packages(exclude=("tests", "tests.*")),
     include_package_data=True,
