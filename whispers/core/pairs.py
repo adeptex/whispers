d8c47a1dbe9bc53a25de1fdeecd2a1d30dafb822
adeptex <adeptex@users.noreply.github.com>
2024-10-05 16:30:15 +0200

    Lazy-load parsers

--- whispers/core/pairs.py
+++ whispers/core/pairs.py
@@ -6,22 +6,6 @@ from whispers.core.constants import REGEX_AST_FILE, REGEX_PRIVKEY_FILE
 from whispers.core.utils import global_exception_handler, is_static, strip_string
 from whispers.models.appconfig import AppConfig
 from whispers.models.pair import KeyValuePair
-from whispers.plugins.config import Config
-from whispers.plugins.dockercfg import Dockercfg
-from whispers.plugins.dockerfile import Dockerfile
-from whispers.plugins.elixir import Elixir
-from whispers.plugins.gradle import Gradle
-from whispers.plugins.html import Html
-from whispers.plugins.htpasswd import Htpasswd
-from whispers.plugins.jproperties import Jproperties
-from whispers.plugins.json import Json
-from whispers.plugins.npmrc import Npmrc
-from whispers.plugins.pip import Pip
-from whispers.plugins.plaintext import Plaintext
-from whispers.plugins.pypirc import Pypirc
-from whispers.plugins.shell import Shell
-from whispers.plugins.xml import Xml
-from whispers.plugins.yml import Yml
 
 
 def make_pairs(config: AppConfig, file: Path) -> Optional[Iterator[KeyValuePair]]:
@@ -115,54 +99,88 @@ def load_plugin(file: Path, ast: bool = False) -> Optional[object]:
         filetype = file_name.split(".")[-1]
 
     if filetype in ["yaml", "yml"]:
+        from whispers.plugins.yml import Yml
+
         return Yml
 
     elif filetype == "json":
+        from whispers.plugins.json import Json
+
         return Json
 
     elif filetype == "xml":
+        from whispers.plugins.xml import Xml
+
         return Xml
 
     elif filetype.startswith("npmrc"):
+        from whispers.plugins.npmrc import Npmrc
+
         return Npmrc
 
     elif filetype.startswith("pypirc"):
+        from whispers.plugins.pypirc import Pypirc
+
         return Pypirc
 
     elif file_name == "pip.conf":
+        from whispers.plugins.pip import Pip
+
         return Pip
 
     elif file_name == "build.gradle":
+        from whispers.plugins.gradle import Gradle
+
         return Gradle
 
     elif filetype in ["conf", "cfg", "cnf", "config", "ini", "credentials", "s3cfg"]:
+        from whispers.plugins.config import Config
+
         return Config
 
     elif filetype == "properties":
+        from whispers.plugins.jproperties import Jproperties
+
         return Jproperties
 
     elif filetype.startswith(("sh", "bash", "zsh", "env")) or file_name == "environment":
+        from whispers.plugins.shell import Shell
+
         return Shell
 
     elif "dockerfile" in file_name:
+        from whispers.plugins.dockerfile import Dockerfile
+
         return Dockerfile
 
     elif filetype == "dockercfg":
+        from whispers.plugins.dockercfg import Dockercfg
+
         return Dockercfg
 
     elif filetype.startswith("htpasswd"):
+        from whispers.plugins.htpasswd import Htpasswd
+
         return Htpasswd
 
     elif filetype == "txt":
+        from whispers.plugins.plaintext import Plaintext
+
         return Plaintext
 
     elif filetype.startswith("htm"):
+        from whispers.plugins.html import Html
+
         return Html
 
     elif filetype == "exs":
+        from whispers.plugins.elixir import Elixir
+
         return Elixir
 
     elif REGEX_PRIVKEY_FILE.match(filetype):
+        from whispers.plugins.plaintext import Plaintext
+
         return Plaintext
 
     elif ast and REGEX_AST_FILE.match(filetype):
